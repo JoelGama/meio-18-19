bb81dcdbdb3fa3539824632503f230685805466b
JoelGama <joelgama2015@gmail.com>
2019-04-26 18:15:53 +0100

    terminar ex1 e fazer ex2

--- Main.java
+++ Main.java
@@ -1,31 +1,39 @@
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
+
+class Order {
+
+    int delivery;
+    int quantity;
+
+    Order(int delivery, int quantity) {
+        this.delivery = delivery;
+        this.quantity = quantity;
+    }
+}
 
 public class Main {
 
     static final Random randGen = new Random();
-    float C1 = 17.37f;
-    float C2 = 22;
-    float C3 = 900;
-    float t = 2;
+    static Main main;
+    double C1 = 17.37;
+    double C2 = 22;
+    double C3 = 900;
+    double b = 96.5;
+    double t = 2;
+    double price = 120;
 
-    int S = 2000;
-    int s = 600;
+    double avg18 = 414.34; // (419.1 * 16 + 554.2 * 12 + 334.6 * 22)/50
 
-    int initialStock = 2000;
-
-    float avg18 = 414.34f; // calcular
-
-    ArrayList<Float> avg2018;
-    ArrayList<Float> cicle2019;
-    ArrayList<Float> stdDev;
-    ArrayList<Float> trend;
+    ArrayList<Double> avg2018;
+    ArrayList<Double> cicle2019;
+    ArrayList<Double> stdDev;
+    ArrayList<Double> trend;
 
     public void add2018() {
         this.avg2018 = new ArrayList<>();
-        this.avg2018.add(419.1f);
-        this.avg2018.add(554.2f);
-        this.avg2018.add(334.6f);
+        this.avg2018.add(419.1);
+        this.avg2018.add(554.2);
+        this.avg2018.add(334.6);
     }
 
     public void calculate2019() {
@@ -35,13 +43,13 @@ public class Main {
         this.cicle2019.add(this.avg2018.get(2)-this.avg18);
 
         this.stdDev = new ArrayList<>();
-        this.stdDev.add(this.cicle2019.get(0)*0.087f);
-        this.stdDev.add(this.cicle2019.get(1)*0.087f);
-        this.stdDev.add(this.cicle2019.get(2)*0.087f);
+        this.stdDev.add(this.cicle2019.get(0)*0.087);
+        this.stdDev.add(this.cicle2019.get(1)*0.087);
+        this.stdDev.add(this.cicle2019.get(2)*0.087);
     }
 
-    public float getTrend(int week) {
-        return this.avg18*(1+(week/50)*0.038f);
+    public double getTrend(int week) {
+        return this.avg18*(1+(week/50)*0.038);
     }
 
     public int randL() {
@@ -64,73 +72,163 @@ public class Main {
         }
     }
 
-    public void round() {
+    public double round(int S, int s) {
+        ArrayList<Integer> stockOnHand = new ArrayList<>();
         ArrayList<Integer> stocks = new ArrayList<>();
-        stocks.add(initialStock);
+        ArrayList<Order> orders = new ArrayList<>();
+        stockOnHand.add(S);
+        stocks.add(S);
+
+        double cost = S*b + C3;
+        double revenue = 0;
 
         for (int i = 1; i <= 50; i++) {
             int bought = 0;
+            int delivered = 0;
             int r = (int) (randGen.nextGaussian() * stdDev.get(this.period(i)) + cicle2019.get(this.period(i)) + this.getTrend(i));
 
-            int l = randL();
-            boolean delivery = (i - l) % 2 == 0;
-            boolean buy = i > 2 && stocks.get(i-l) < this.s;
-            if (delivery && buy) {
-                bought = this.S - stocks.get(i-l);
+            for (Order o : orders) {
+                o.delivery--;
+                if (o.delivery == 0) {
+                    delivered += o.quantity;
+                }
             }
 
-            int newStock = stocks.get(i-1) - r * l + bought;
+            for (int j = 0; j < orders.size(); j++) {
+                if (orders.get(j).delivery < 0) {
+                    orders.remove(j);
+                }
+            }
 
+            int l = randL();
+            boolean delivery = i % 2 == 0;
+            boolean buy = stockOnHand.get(i-1) < s;
+            if (delivery && buy) {
+                bought = S - stockOnHand.get(i-1);
+                orders.add(new Order(l+1, bought));
+            }
+
+            int newStockOnHand = stockOnHand.get(i-1) - r + bought;
+            int newStock = stocks.get(i-1) - r + delivered;
+            stockOnHand.add(newStockOnHand);
             stocks.add(newStock);
 
-            System.out.println("Stock " + i + ": " + newStock + ". Recebeu? " + buy + " " + l);
+            revenue += r * price;
+            cost += bought * b + (bought > 0 ? C3 : 0);
+            if (newStock < 0) {
+                cost += -newStock * C2;
+            }
+
+            //System.out.println(i + ": " + " Stock   " + newStock + "    stockOnHand " + newStockOnHand + "    l " + l);
         }
 
-        System.out.println("Final Cost: " + this.cost(stocks));
+        int total = 0;
+        int i;
+        for (i = 1; i < stocks.size() ; i++) {
+            int stock = stocks.get(i);
+            if (stock > 0) {
+                total += stock;
+            }
+        }
 
+        cost += C1 * ((double) total) / (i-1);
+        //System.out.println("Cost: " + cost + " Revenue: " + revenue + " Profit: " + (revenue - cost));
+
+        return (revenue - cost); //profit
     }
 
-    private float cost(ArrayList<Integer> stocks) {
+    private double cost(ArrayList<Integer> stocks) {
         int sum = 0;
-        float lost;
+        double lost;
 
         for (int i = 1; i < stocks.size(); i++) {
             if (stocks.get(i) >= 0) sum += stocks.get(i);
             else {
                 if (stocks.get(i-1) >= 0) {
-                    float wallet = root(i - 1, i, stocks.get(i - 1), stocks.get(i)) - (i - 1);
+                    double wallet = root(i - 1, i, stocks.get(i - 1), stocks.get(i)) - (i - 1);
                     lost = - wallet * stocks.get(i) / 2;
                 } else {
-                    lost = - (float)(stocks.get(i) + stocks.get(i-1)) / 2;
+                    lost = - (double)(stocks.get(i) + stocks.get(i-1)) / 2;
                 }
 
-                System.out.println("Lost: " + i + " " + lost);
+                //System.out.println("Lost: " + i + " " + lost);
             }
         }
 
-        float avgStock = ((float) sum) / stocks.size();
+        double avgStock = ((double) sum) / stocks.size();
 
         return avgStock;
 
     }
 
-    private float root(Integer a, Integer b, Integer fa, Integer fb) {
+    private double root(Integer a, Integer b, Integer fa, Integer fb) {
 
-        Float m = ((float) fb - fa) / (b - a);
+        Double m = ((double) fb - fa) / (b - a);
 
-        float c = fb - b * m;
+        double c = fb - b * m;
 
         return -c / m;
     }
 
+    private double avgProfit(int S, int s, int numRounds) {
+
+        double profit = 0;
+        for (int i = 0; i < numRounds; i++) {
+            profit += round(S, s);   
+        }
+
+        System.out.println("AvgProfit: " + profit/numRounds);
+        return profit/numRounds;
+    }
+
     public static void main(String[] args) {
 
-        Main main = new Main();
+        int numRounds = 100000;
+
+        main = new Main();
 
         main.add2018();
         main.calculate2019();
 
-        main.round();
+        int S = 6000;//randGen.nextInt(10000);
+        int s = 900;//S - randGen.nextInt(S);
+        double p = main.avgProfit(S, s, numRounds);
+        for (int i = 0; i < 50; i++) {
+            int newS = randGen.nextInt(10000);
+            int news = newS - randGen.nextInt(newS);
+            System.out.println("S " + newS + " s " + news);
+            double newp = main.avgProfit(newS, news, numRounds);
+            if (newp > p) {
+                p = newp;
+                S = newS;
+                s = news;
+            }
+        }
+        
+        System.out.println();
 
+        Double ps[] = new Double[4];
+        int changeS[] = {50, -50, 0, 0};
+        int changes[] = {0, 0, 50, -50};
+        int done = 10;
+        while (done != 0) {
+            ps[0] = main.avgProfit(S+50, s, numRounds);
+            ps[1] = main.avgProfit(S-50, s, numRounds);
+            ps[2] = main.avgProfit(S, s+50, numRounds);
+            ps[3] = main.avgProfit(S, s-50, numRounds);
+            List<Double> l = Arrays.asList(ps);
+            double max = Collections.max(l);
+            int maxIndex = l.indexOf(max);
+            if (max > p) {
+                p = max;
+                S += changeS[maxIndex];
+                s += changes[maxIndex];
+                done = 10;
+            } else {
+                done--;
+            }
+            System.out.println("S " + S + " s " + s + " done " + done);
+        }
+        System.out.println("Profit: " + p);
     }
 }
