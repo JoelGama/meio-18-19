1459348ba89a659888d2430969d9005e76910064
Alexandre Pinho <lxpnh98@gmail.com>
2019-05-08 23:49:00 +0100

    Adicionar nível de serviço (número de semanas com stock positivo); aumentar semanas para 52.

--- Main.java
+++ Main.java
@@ -11,10 +11,22 @@ class Order {
     }
 }
 
+class Result {
+
+    double profit;
+    double service;
+
+    Result(double profit, double service) {
+        this.profit = profit;
+        this.service = service;
+    }
+}
+
 public class Main {
 
     static final Random randGen = new Random();
     static Main main;
+    static int weeks = 52;
     double C1 = 17.37;
     double C2 = 22;
     double C3 = 900;
@@ -22,7 +34,7 @@ public class Main {
     double t = 2;
     double price = 120;
 
-    double avg18 = (419.1 * 16 + 554.2 * 12 + 334.6 * 22)/50;
+    double avg18 = (419.1 * 16 + 554.2 * 12 + 334.6 * 22)/weeks;
 
     ArrayList<Double> avg2018;
     ArrayList<Double> cicle2019;
@@ -59,7 +71,7 @@ public class Main {
             return 1;
         }
 
-        return 2;           
+        return 2;
     }
 
     private int period(int week) {
@@ -72,7 +84,7 @@ public class Main {
         }
     }
 
-    public double round(int S, int s) {
+    public Result round(int S, int s) {
         ArrayList<Integer> stockOnHand = new ArrayList<>();
         ArrayList<Integer> stocks = new ArrayList<>();
         ArrayList<Order> orders = new ArrayList<>();
@@ -82,7 +94,7 @@ public class Main {
         double cost = S*b + C3;
         double revenue = 0;
 
-        for (int i = 1; i <= 50; i++) {
+        for (int i = 1; i <= weeks; i++) {
             int bought = 0;
             int delivered = 0;
             int r = (int) (randGen.nextGaussian() * stdDev.get(this.period(i)) + cicle2019.get(this.period(i)) + this.getTrend(i));
@@ -123,13 +135,18 @@ public class Main {
         }
 
         int total = 0;
+        int out_of_stock_count = 0;
         int i;
         for (i = 1; i < stocks.size() ; i++) {
             int stock = stocks.get(i);
             if (stock > 0) {
                 total += stock;
+            } else {
+                out_of_stock_count++;
             }
         }
+
+        double service = 1 - ((double)out_of_stock_count)/weeks;
 
         int lastStock = stocks.get(i-1);
         if (lastStock < 0) {
@@ -140,20 +157,34 @@ public class Main {
         }
 
         cost += C1 * ((double) total) / (i-1);
-        //System.out.println("Cost: " + cost + " Revenue: " + revenue + " Profit: " + (revenue - cost));
+        double profit = revenue - cost;
+        //System.out.println("Cost: " + cost + " Revenue: " + revenue + " Profit: " + profit + " Service: " + service);
 
-        return (revenue - cost); //profit
+        Result r = new Result(profit, service);
+
+        return r;
     }
 
     private double avgProfit(int S, int s, int numRounds) {
 
         double profit = 0;
         for (int i = 0; i < numRounds; i++) {
-            profit += round(S, s);   
+            profit += round(S, s).profit;
         }
 
-        System.out.println("AvgProfit: " + profit/numRounds);
+        //System.out.println("AvgProfit: " + profit/numRounds);
         return profit/numRounds;
+    }
+
+    private double avgService(int S, int s, int numRounds) {
+
+        double service = 0;
+        for (int i = 0; i < numRounds; i++) {
+            service += round(S, s).service;
+        }
+
+        //System.out.println("AvgService: " + service/numRounds);
+        return service/numRounds;
     }
 
     private void hillClimbing() {
@@ -166,18 +197,18 @@ public class Main {
         for (int i = 0; i < 5000; i++) {
             int newS = randGen.nextInt(10000);
             int news = newS - randGen.nextInt(newS+1);
-            System.out.println("S " + newS + " s " + news);
             double newp = main.avgProfit(newS, news, numRounds);
+            System.out.println("S " + newS + " s " + news);
             if (newp > p) {
                 p = newp;
                 S = newS;
                 s = news;
             }
         }
-        
+
         System.out.println();
 
-        numRounds = 50000;
+        numRounds = 10000;
         p = main.avgProfit(S, s, numRounds);
         System.out.println("S " + S + " s " + s);
         System.out.println();
@@ -209,16 +240,38 @@ public class Main {
             }
             System.out.println("S " + S + " s " + s + " tries " + tries);
         }
-        System.out.println("Profit: " + p);
+
+        // round to tens
+        S = (S+5)/10*10;
+        s = (s+5)/10*10;
+
+        numRounds = 500000;
+        System.out.println("\nResults:");
+        System.out.println("S " + S + " s " + s);
+        System.out.println("Profit (optimal): " + main.avgProfit(S, s, numRounds));
+        System.out.println("Service: " + main.avgService(S, s, numRounds));
+
+        for (int i = 0; i < ps.length; i++) {
+            double alt_profit = main.avgProfit(S+changeS[i]*10, s+changes[i]*10, numRounds);
+            double alt_service = main.avgService(S+changeS[i]*10, s+changes[i]*10, numRounds);
+            System.out.println("S " + (S+changeS[i]*10) + " s " + (s+changes[i]*10));
+            System.out.println("Profit: " + alt_profit + " Service: " + alt_service);
+        }
     }
 
     private void grid(int minS, int maxS, int SStep, int mins, int maxs, int sStep) {
         int numRounds = 5000;
+        for (int S = minS; S <= maxS; S+=SStep) {
+            System.out.print(""+S+(S == maxS ? "" : ","));
+        }
+        System.out.println();
+
         for (int s = mins; s <= maxs; s+=sStep) {
+            System.out.print(""+s+",");
             for (int S = minS; S <= maxS; S+=SStep) {
                 //System.out.println("S " + S + " s " + s);
                 double p = avgProfit(S, s, numRounds);
-                System.out.print(""+p+",");
+                System.out.print(""+p+(S == maxS ? "" : ","));
             }
             System.out.println();
         }
@@ -231,8 +284,8 @@ public class Main {
         main.add2018();
         main.calculate2019();
 
-        //main.grid(0, 10000, 100, 0, 10000, 100);
-        main.hillClimbing();
+        main.grid(0, 15000, 100, 0, 10000, 100);
+        //main.hillClimbing();
 
     }
 }
